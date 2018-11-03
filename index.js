7a9566802760c5589540983c4589575d040b7cd8
coolguy284 <santoshschaefer@gmail.com>
2018-11-03 12:04:00 -0400

    Basic Addition

---
+++ index.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+  res.write('test: ' + req.url);
+  res.end();
+}).listen(8080, function () {console.log('e')});
