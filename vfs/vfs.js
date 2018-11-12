14ad477e0c8b8c2fa46409ff72e59ca599d18dfb
coolguy284 <coolguy284v64@gmail.com>
2018-11-11 21:48:01 -0500

    Update vfs

--- vfs/vfs.js
+++ vfs/vfs.js
@@ -1,18 +1,24 @@
 // jshint -W041
+function getcTime() {
+  return new Date().getTime();
+}
+function parentPath(path) {
+  if (path == '/') return '/';
+  let patharr = path.split('/');
+  return patharr.slice(0, patharr.length - 1).join('/');
+}
 class FileSystem {
-  constructor(writable, inoarr, inodarr, fi) {
-    if (writable === undefined) {
-      writable = false;
-    }
+  constructor(writable, inoarr, inodarr, fi, groups) {
+    if (writable === undefined) writable = false;
     if (inoarr === undefined) {
-      inoarr = [new Buffer(0)];
+      inoarr = [Buffer.alloc(0)];
     }
     if (inodarr === undefined) {
-      inodarr = [['d']];
+      let ctime = getcTime();
+      inodarr = [['d', 1, ctime, ctime, ctime, 0o777, 'root', 'root']];
     }
-    if (fi === undefined) {
-      fi = [];
-    }
+    if (fi === undefined) fi = [];
+    if (groups === undefined) groups = {};
     this.writable = writable;
     this.inoarr = inoarr;
     this.inodarr = inodarr;
@@ -28,8 +34,9 @@ class FileSystem {
     if (this.fi.length > 0) {
       return this.fi.splice(0, 1)[0];
     } else {
-      this.inoarr.push(new Buffer(0));
-      this.inodarr.push([typ]);
+      let ctime = getcTime();
+      this.inoarr.push(Buffer.alloc(0));
+      this.inodarr.push([typ, 0, ctime, ctime, ctime, 0o777, 'root', 'root']);
       return this.inoarr.length - 1;
     }
   }
@@ -64,10 +71,18 @@ class FileSystem {
       return ino;
     }
   }
+  geteInode(path) {
+    let rv = this.getInode(path);
+    if (rv === null) {
+      throw new Error('ENOENT no such file or directory: ' + path);
+    }
+    return rv;
+  }
   createFile(path, typ) {
     let patharr = path.split('/');
     let ino = this.popfi(typ);
-    this.appendFileSync(patharr.slice(0, patharr.length - 1).join('/'), new Buffer(patharr[patharr.length - 1] + ':' + ino));
+    this.appendFolder(this.geteInode(parentPath(path)), patharr[patharr.length - 1], ino);
+    this.inodarr[ino][1]++;
     return ino;
   }
   getcInode(path, typ) {
@@ -77,31 +92,63 @@ class FileSystem {
       return this.createFile(path, typ);
     }
   }
-  existsSync(path) {
-    return this.getInode(path) != null;
-  }
-  readFileSync(path) {
-    let ino = this.getInode(path);
-    if (ino == null) {
-      throw new Error('ENOENT no such file or directory: ' + patharr.slice(0, i).join('/'));
-    }
-    return new Buffer(this.inoarr[ino]);
-  }
-  writeFileSync(path, buf) {
+  appendFolder(ino, nam, inot) {
     if (this.writable) {
-      if (typeof buf == 'string') {
-        buf = new Buffer(buf);
-      }
-      let ino = this.getcInode(path, 'f');
-      this.inoarr[ino] = new Buffer(buf);
+      this.inoarr[ino] = Buffer.concat([this.inoarr[ino], Buffer.from(nam + ':' + inot)]);
     } else {
       throw new Error('read-only filesystem');
     }
   }
-  appendFileSync(path, buf) {
+  exists(path) {
+    return this.getInode(path) != null;
+  }
+  chmod(path, mode) {
+    if (!this.writable) {
+      throw new Error('read-only filesystem');
+    }
+    if (mode >= 0 && mode <= 0o777) {
+      let ino = this.geteInode(path);
+      this.inodarr[ino][5] = mode;
+    }
+  }
+  chown(path, user, group) {
+    if (!this.writable) {
+      throw new Error('read-only filesystem');
+    }
+    let ino = this.geteInode(path);
+    this.inodarr[ino][6] = user;
+    this.inodarr[ino][7] = user;
+  }
+  readFile(path) {
+    let ino = this.geteInode(path);
+    return Buffer.from(this.inoarr[ino]);
+  }
+  writeFile(path, buf) {
+    if (this.writable) {
+      let ino = this.getcInode(path, 'f');
+      this.inoarr[ino] = Buffer.from(buf);
+    } else {
+      throw new Error('read-only filesystem');
+    }
+  }
+  createReadStream(path) {
+    let s = new datajs.s.BufReadStream(this.inoarr[this.geteInode(path)]);
+    return s;
+  }
+  createWriteStream(path) {
+    if (!this.writable) {
+      throw new Error('read-only filesystem');
+    }
+    let s = new datajs.s.BufWriteStream(undefined, true);
+    s.on('finish', function () {
+      this.inoarr[this.getcInode(path, 'f')] = s.ibuf;
+    });
+    return s;
+  }
+  appendFile(path, buf) {
     if (this.writable) {
       if (typeof buf == 'string') {
-        buf = new Buffer(buf);
+        buf = Buffer.from(buf);
       }
       let ino = this.getcInode(path, 'f');
       this.inoarr[ino] = Buffer.concat([this.inoarr[ino], buf]);
@@ -109,21 +156,70 @@ class FileSystem {
       throw new Error('read-only filesystem');
     }
   }
-  copySync(pathf, patht) {
+  copyFile(pathf, patht) {
     if (this.writable) {
-      let inof = this.getInode(pathf);
-      if (inof == null) {
-        throw new Error('ENOENT no such file or directory: ' + patharr.slice(0, i).join('/'));
-      }
-      let inot = this.getcInode(patht);
-      this.inoarr[inot] = new Buffer(this.inoarr[inof]);
+      let inof = this.geteInode(pathf);
+      let inot = this.getcInode(patht, 'f');
+      this.inoarr[inot] = Buffer.from(this.inoarr[inof]);
     } else {
       throw new Error('read-only filesystem');
     }
   }
+  link(pathf, patht) {
+    let ino = this.geteInode(pathf);
+    let patharr = patht.split('/');
+    if (this.exists(patht)) throw new Error('path already exists');
+    this.appendFolder(this.geteInode(parentPath(patht)), patharr[patharr.length - 1], ino);
+    this.inodarr[ino][1]++;
+  }
 }
+class FileSystemView {
+  constructor(fs, cwd, user) {
+    if (cwd === undefined) cwd = '';
+    if (user === undefined) user = 'root';
+    this.fs = fs;
+    this.cwd = cwd;
+    this.user = user;
+  }
+  getPerms(ino, user) {
+    if (ino === null) {
+      return {read: 0, write: 0, execute: 0};
+    }
+    let pl = this.fs.inodarr[ino][5];
+    return {};
+  }
+  normalize(path) {
+    if (path[0] != '/') path = this.cwd + path;
+    let patharr = path.split('/');
+    let bp = [];
+    for (let i in patharr) {
+      
+    }
+  }
+  existsSync(path) {
+    path = this.normalize(path);
+    this.getPerms(this.fs.getInode(parentPath(path)));
+  }
+}
+function SecureView(view) {
+  return {
+    get cwd() {
+      return view.cwd;
+    },
+    existsSync: view.existsSync
+  };
+}
+let rawfs = new FileSystem(true);
+let fs = new FileSystemView(rawfs);
 module.exports = {
-  'FileSystem' : FileSystem,
-  'fs' : new FileSystem(true)
+  helperf: {
+    getcTime: getcTime,
+    parentPath: parentPath
+  },
+  FileSystem: FileSystem,
+  FileSystemView: FileSystemView,
+  SecureView: SecureView,
+  rawfs: rawfs,
+  fs: fs,
 };
-module.exports.fs.writeFileSync('/tex.txt', new Buffer('ell'));
+rawfs.writeFile('/tex.txt', Buffer.from('ell'));
