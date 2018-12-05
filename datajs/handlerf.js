3ac292f0a61b9801301f8fbe51c2465e12343664
coolguy284 <coolguy284v64@gmail.com>
2018-12-05 18:36:51 -0500

    Consolidate calculator

---
+++ datajs/handlerf.js
@@ -0,0 +1,18 @@
+module.exports = {
+  '/dat.html' : new Function('req', 'res', 'res.writeHead(200,{"Content-Type":"text/plain; charset=utf-8"});res.write(new Date().toString());res.end();'),
+  '/calculator.js' : function (req, res) {
+    res.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'});
+    let v = Buffer.from('\n');
+    res.write(Buffer.concat([fs.readFileSync('websites/js/utilinspect.js'), v]));
+    res.write(Buffer.concat([fs.readFileSync('websites/calculator/constants.js'), v]));
+    res.write(Buffer.concat([fs.readFileSync('websites/calculator/types.js'), v]));
+    res.write(Buffer.concat([fs.readFileSync('websites/calculator/operators.js'), v]));
+    res.write(Buffer.concat([fs.readFileSync('websites/calculator/funccall.js'), v]));
+    res.write(Buffer.concat([fs.readFileSync('websites/calculator/namespace.js'), v]));
+    res.write(Buffer.concat([fs.readFileSync('websites/calculator/convert.js'), v]));
+    res.write(Buffer.concat([fs.readFileSync('websites/calculator/parser.js'), v]));
+    res.write(fs.readFileSync('websites/calculator/index.js'));
+    res.end();
+    return -1;
+  },
+};
